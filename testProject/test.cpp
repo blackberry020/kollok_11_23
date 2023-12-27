d73e55fd1b13ea280b50e9f55174bed292937e06
blackberry020 <ilonafrolova00@gmail.com>
2023-12-27 18:10:16 +0300

    test done

--- testProject/test.cpp
+++ testProject/test.cpp
@@ -1,6 +1,4 @@
 #include "pch.h"
-//#include "../kollok_11_23/kollok_11_23.cpp"
-//#include "stdafx.h"
 #include "gtest/gtest.h"
 
 #include "../kollok_11_23/matrixFunctions.h"
@@ -10,16 +8,39 @@ class transposeMatrixTest : public ::testing::Test {
 
 TEST(transposeMatrixTest, TransposeOneElementMatrix) {
 
-	double** oneElementMatrix = createMatrix<double>(1, 1);
-	oneElementMatrix[0][0] = 5;
-	double** transposed = transposeMatrix<double>(oneElementMatrix, 1, 1);
+	int n = 1, m = 1;
 
-	double** expected = createMatrix<double>(1, 1);
+	double** oneElementMatrix = createMatrix<double>(n, m);
+	oneElementMatrix[0][0] = 5;
+	double** transposed = transposeMatrix<double>(oneElementMatrix, n, m);
+
+	double** expected = createMatrix<double>(n, m);
 	expected[0][0] = 5;
 
-	//ASSERT_EQ(memcmp(expected, transposed, sizeof(expected)), 0);
-	ASSERT_TRUE(true);
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++)
+			EXPECT_EQ(oneElementMatrix[i][j], transposed[i][j]);
+	}
 }
+
+//TEST(transposeMatrixTest, TransposeTwoByThreeMatrix) {
+//
+//	int n = 2, m = 3;
+//
+//	
+//
+//	double** oneElementMatrix = createMatrix<double>(n, m);
+//	oneElementMatrix[0][0] = 5;
+//	double** transposed = transposeMatrix<double>(oneElementMatrix, n, m);
+//
+//	double** expected = createMatrix<double>(n, m);
+//	expected[0][0] = 5;
+//
+//	for (int i = 0; i < n; i++) {
+//		for (int j = 0; j < m; j++)
+//			EXPECT_EQ(oneElementMatrix[i][j], transposed[i][j]);
+//	}
+//}
 
 int main(int argc, char** argv) {
 	::testing::InitGoogleTest(&argc, argv);
